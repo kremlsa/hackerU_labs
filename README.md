bff3296a5ebd42d78bcea3213c808fe87b202894
Aleksandr Kremlev <kremlsa@yandex.ru>
2022-10-03 21:48:40 +0700

    fix

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # hackerU_labs
+Исправления в классе CWE78_OS_Command_Injection__getParameter_Servlet_10
+
+помечены:
+
+/* FIX FLAW: command injection */
---
+++ test/testcases/CWE78_OS_Command_Injection/CWE78_OS_Command_Injection__getParameter_Servlet_10Test.java
@@ -0,0 +1,9 @@
+package testcases.CWE78_OS_Command_Injection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CWE78_OS_Command_Injection__getParameter_Servlet_10Test {
+    CWE78_OS_Command_Injection__getParameter_Servlet_10 cwe = new CWE78_OS_Command_Injection__getParameter_Servlet_10();
+
+
+}
